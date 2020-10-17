98471b35441bd02fd4e18d4698daa5b508c9808b
Sandy Raveloarisoa <sandy.rav@onja.org>
2020-10-17 09:18:50 +0300

    added some start css and set up react components

---
+++ webroot/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Team from './Team';
+
+function App() {
+    return(
+       <div>
+           <Team />
+       </div>
+    )
+}
+
+export default App;
---
+++ webroot/components/Team.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Team() {
+    return (
+        <h2>Hi there</h2>
+    )
+}
+
+export default Team;
